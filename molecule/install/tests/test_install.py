7d61ec26c0389d71db8c1313481c1731355a8c3b
Ben Crisp <ben@thecrisp.io>
2021-01-10 03:35:37 +0000

    simplify instances and tests

---
+++ molecule/install/tests/test_install.py
@@ -0,0 +1,16 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+INVENTORY = os.environ['MOLECULE_INVENTORY_FILE']
+HOST_PATTERN = "all"
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    INVENTORY).get_hosts(HOST_PATTERN)
+
+
+def test_postfix_package_is_installed(host):
+    if host.system_info.distribution in ["debian"]:
+        postfix_package = "postfix"
+    pkg = host.package(postfix_package)
+    assert pkg.is_installed
